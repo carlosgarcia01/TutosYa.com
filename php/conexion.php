55f1beb3c2acc57b29f4c50ea791e279c35c9c79
carlos <cg94947@gmail.com>
2019-04-07 16:52:23 -0500

    pagina inicio

--- php/conexion.php
+++ php/conexion.php
@@ -2,4 +2,10 @@
 
 $conn=mysqli_connect("127.0.01","root","carlosg","tutosya");
 
+    if($conn->connet_error)
+        die($conn->conneet_error);
+
+    
+    print('Conexion exitosa \n');
+    exit(1);
 ?>
