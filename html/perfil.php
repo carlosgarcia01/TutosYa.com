01fa1ee087d27a6f9664521ba51315a044cc185d
carlos <cg94947@gmail.com>
2019-04-27 14:53:13 -0500

    perfil

--- html/perfil.php
+++ html/perfil.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<?php session_start();?>  
+<?php session_start();?> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/perfil.css">
+    <link rel="stylesheet" href="../css/perfil.css" >
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
